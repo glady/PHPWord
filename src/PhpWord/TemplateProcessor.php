992fd0e170c52067d742948cf11caf8dc824470a
neopheus <neopheus@gmail.com>
2022-09-12 12:56:45 +0200

    Fix #2290

--- src/PhpWord/TemplateProcessor.php
+++ src/PhpWord/TemplateProcessor.php
@@ -265,7 +265,7 @@ class TemplateProcessor
      */
     protected static function ensureUtf8Encoded($subject)
     {
-        if (!Text::isUTF8($subject)) {
+        if (!Text::isUTF8($subject) && !is_null($subject)) {
             $subject = utf8_encode($subject);
         }
 
