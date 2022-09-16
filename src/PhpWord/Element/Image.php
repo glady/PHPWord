ef99e56ffb5a695b8eb236cc641eed3f20baa7c1
Adrien Crivelli <adrien.crivelli@gmail.com>
2022-09-16 10:17:31 +0200

    Drop PHP 7.3 and older
    This is according to our formal, published, policy to only support
    eol PHP after 6 months within PHPOffice organisation.
    
    See https://phpspreadsheet.readthedocs.io/en/latest/#php-version-support

--- src/PhpWord/Element/Image.php
+++ src/PhpWord/Element/Image.php
@@ -418,7 +418,7 @@ class Image extends AbstractElement
             // WMF image is dimensionless
             $imageData = array(null, null, self::IMAGETYPE_WMF);
         } elseif ($this->sourceType == self::SOURCE_STRING) {
-            $imageData = $this->getStringImageSize($this->source);
+            $imageData = @getimagesizefromstring($this->source);
         } else {
             $imageData = @getimagesize($this->source);
         }
@@ -507,26 +507,6 @@ class Image extends AbstractElement
         }
 
         return $imageData;
-    }
-
-    /**
-     * get image size from string
-     *
-     * @param string $source
-     *
-     * @codeCoverageIgnore this method is just a replacement for getimagesizefromstring which exists only as of PHP 5.4
-     */
-    private function getStringImageSize($source)
-    {
-        $result = false;
-        if (!function_exists('getimagesizefromstring')) {
-            $uri = 'data://application/octet-stream;base64,' . base64_encode($source);
-            $result = @getimagesize($uri);
-        } else {
-            $result = @getimagesizefromstring($source);
-        }
-
-        return $result;
     }
 
     /**
